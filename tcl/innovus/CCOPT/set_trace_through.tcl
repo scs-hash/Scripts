b31b1a8d6237b5ac9ba94548183699e259047133
scs <lavallcw@gmail.com>
2023-08-16 13:40:59 +0800

    Update unset_ccopt_property command

--- tcl/innovus/CCOPT/set_trace_through.tcl
+++ tcl/innovus/CCOPT/set_trace_through.tcl
@@ -2,5 +2,4 @@
 set_ccopt_property trace_through_to -pin ${inst}/CP ${inst}/Q
 
 # 2. unset ccopt property
-unset_ccopt_property trace_through_to -pin ${inst}/CP ${inst}/Q
-
+unset_ccopt_property trace_through_to -pin ${inst}/CP
